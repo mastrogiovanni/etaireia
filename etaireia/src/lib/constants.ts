0cefa5f52ce8f469c8b3ea20edc99a3c8b0a9252
Michele Mastrogiovanni <michele.mastrogiovanni@elerian.ai>
2023-09-28 00:42:52 +0200

    fix: test

--- etaireia/src/lib/constants.ts
+++
@@ -1,5 +0,0 @@
-// In dev mode
-export let baseUrl = "http://localhost"
-
-// In prod mode
-// export let baseUrl = ""
--- etaireia/workbox-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	globDirectory: 'static/',
-	globPatterns: [
-		'**/*.{webmanifest,png,pdf,jpg,html,js,ts}'
-	],
-	swDest: 'static/sw.js',
-	swSrc: 'sw.js'
-};
