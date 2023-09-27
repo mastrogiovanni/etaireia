077c705afcd71833c01b14a1bd49b313b48c1d47
Michele Mastrogiovanni <michele.mastrogiovanni@elerian.ai>
2023-09-28 00:43:24 +0200

    fix: test

--- etaireia-backend/internal/server/models/signature.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "mime/multipart"
-
-type SignRequest struct {
-	Document  *multipart.FileHeader `form:"document" binding:"required"`
-	Signature string                `form:"signature" binding:"required"`
-	PublicKey string                `form:"publicKey" binding:"required"`
-}
